3d4c4f32daae35e943f510395a9f3aed5830a550
gupta <>
2022-03-05 13:52:28 -0500

    Add plot

---
+++ canvas.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(ggplot2)
+
+# Goal: compare how the percent of population working from home has changed
+# over time by education level
+
+# Read in sample data
+map48_sample_data <- readr::read_csv("data-raw/time_series_sample_data.csv", col_types = list(readr::col_date(format = "%m/%d/%y")))
+
+# Format for chart consumption
+remote_work_by_ed_level <- map48_sample_data %>%
+  tidyr::pivot_longer(
+    cols = !date,
+    names_to = "education_level",
+    values_to = "pct_working_remotely"
+  )
+
+# ggplot with default styling
+fig <- remote_work_by_ed_level %>%
+  ggplot(aes(date, pct_working_remotely, group = education_level, color = education_level)) +
+  geom_line() +
+  xlab("")
+
+fig
--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -18,7 +18,17 @@ usethis::use_data(population_sample_data, overwrite = TRUE)
 # time_series_sample_data dataset ------------------------------------------------------
 
 time_series_sample_data <- readr::read_csv("data_raw/time_series_sample_data.csv")
-time_series_sample_data<- time_series_sample_data
 time_series_sample_data$date<- as.Date(time_series_sample_data$date, "%m/%d/%y")
 
 usethis::use_data(time_series_sample_data, overwrite = TRUE)
+
+# remote_work_by_ed_level dataset ------------------------------------------------------
+
+remote_work_by_ed_level <- time_series_sample_data %>%
+  tidyr::pivot_longer(
+    cols = !date,
+    names_to = "education_level",
+    values_to = "pct_working_remotely"
+  )
+
+usethis::use_data(remote_work_by_ed_level, overwrite = TRUE)
