63f36225b4d5ad6a5e5db2d6734d452adfc5ae9a
Julia Silge <julia.silge@gmail.com>
2022-03-05 13:18:31 -0700

    Set data type for `date`

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -11,13 +11,13 @@ usethis::use_data(cori_network_communities, overwrite = TRUE)
 
 # population_sample_data dataset ------------------------------------------------------
 
-population_sample_data <- readr::read_csv("data_raw/population_sample_data.csv")
+population_sample_data <- readr::read_csv("data-raw/population_sample_data.csv")
 
 usethis::use_data(population_sample_data, overwrite = TRUE)
 
 # time_series_sample_data dataset ------------------------------------------------------
 
-time_series_sample_data <- readr::read_csv("data_raw/time_series_sample_data.csv")
+time_series_sample_data <- readr::read_csv("data-raw/time_series_sample_data.csv")
 time_series_sample_data$date<- as.Date(time_series_sample_data$date, "%m/%d/%y")
 
 usethis::use_data(time_series_sample_data, overwrite = TRUE)
@@ -29,6 +29,7 @@ remote_work_by_ed_level <- time_series_sample_data %>%
     cols = !date,
     names_to = "education_level",
     values_to = "pct_working_remotely"
-  )
+  ) %>%
+  mutate(date = lubridate::mdy(date))
 
 usethis::use_data(remote_work_by_ed_level, overwrite = TRUE)
