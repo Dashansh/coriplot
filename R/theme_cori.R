f84899b35103ab16fe4d862572b8b39ea0cdb821
Dashansh <dashansh@yahoo.in>
2022-03-05 14:43:30 -0500

    refurnished the theme

--- R/theme_cori.R
+++ R/theme_cori.R
@@ -16,35 +16,40 @@ theme_cori <- function(base_size = 8.5, base_family = "Times New Roman",
 
   half_line <- base_size / 2L
 
-  ret <- ggplot2::theme(
-    line = ggplot2::element_line(colour = "#000000",
-                                 size = base_line_size,
-                                 linetype = 1L,
-                                 lineend = "butt"),
-    rect = ggplot2::element_rect(fill = "#FFFFFF",
-                                 colour = "#000000",
-                                 size = base_rect_size,
-                                 linetype = 1L),
-    text = ggplot2::element_text(family = base_family,
-                                 face = "plain",
-                                 colour = "#000000",
-                                 size = base_size,
-                                 hjust = 0.5,
-                                 vjust = 0.5,
-                                 angle = 0,
-                                 lineheight = 0.9,
-                                 margin = ggplot2::margin(),
-                                 debug = FALSE),
-    panel.grid.major.x = ggplot2::element_blank(),
-    panel.grid.minor.x = ggplot2::element_blank(),
-    panel.grid.minor.y = ggplot2::element_blank(),
-    panel.grid.major.y = ggplot2::element_line(linetype = "dashed", color = "grey"),
-    panel.border = ggplot2::element_blank(),
-    panel.background = ggplot2::element_blank(),
+  ret <- ggplot2::theme_minimal(base_family = "IBMPlexSans",
+                                base_size = base_size)
 
-    axis.line.x = ggplot2::element_line(color = "grey", linetype = "solid")
+  ret$line = ggplot2::element_line(colour = "#000000",
+                                   size = base_line_size,
+                                   linetype = 1L,
+                                   lineend = "butt")
 
-  )
+  ret$rect = ggplot2::element_rect(fill = "#FFFFFF",
+                                   colour = "#000000",
+                                   size = base_rect_size,
+                                   linetype = 1L)
+
+  ret$text = ggplot2::element_text(family = base_family,
+                                   face = "plain",
+                                   colour = "#000000",
+                                   size = base_size,
+                                   hjust = 0.5,
+                                   vjust = 0.5,
+                                   angle = 0,
+                                   lineheight = 0.9,
+                                   margin = ggplot2::margin(),
+                                   debug = FALSE)
+
+  ret$panel.grid.major.x = ggplot2::element_blank()
+  ret$panel.grid.minor.x = ggplot2::element_blank()
+  ret$panel.grid.minor.y = ggplot2::element_blank()
+  ret$panel.grid.major.y = ggplot2::element_line(linetype = "dashed",
+                                                 color = "grey")
+
+  ret$panel.border = ggplot2::element_blank()
+  ret$panel.background = ggplot2::element_blank()
+
+  ret$axis.line.x = ggplot2::element_line(color = "grey", linetype = "solid")
 
   ret
 }
