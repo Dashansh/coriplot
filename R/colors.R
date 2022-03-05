782d7b07fda70290b8b31ab8efb328ab34896ab4
Abhishek Sinha <abhisheksinha@Abhisheks-MacBook-Pro.local>
2022-03-04 23:03:45 -0500

    adding colors to palette

--- R/colors.R
+++ R/colors.R
@@ -8,6 +8,7 @@
 #'
 #'
 #' @family urbn palettes
+#'
 #' @rdname urbn_palettes
 #' @export
 cori_primary <- c(
@@ -17,115 +18,23 @@ cori_primary <- c(
   `mid orange` = "#FA804A",
   `mid blue` =  "#3E80EA",
   `mid purple` = "#7658A2",
-  `cif mid blue` = "#06BCCB")
+  `cif mid blue` = "#06BCCB"
+)
 
 #' @rdname urbn_palettes
 #' @export
-cori_blue <- c(
-  "#ca5800",
-  "#fdbf11",
-  "#fdd870",
-  "#fff2cf",
-  "#cfe8f3",
-  "#73bfe2",
-  "#1696d2",
-  "#0a4c6a")
+cori_orange <- c(
+  `Rin Orange` = "#E74F2A",
+  `Light Orange` = "#FECEB8"
+)
 
 #' @rdname urbn_palettes
 #' @export
-palette_urbn_quintile <- c(
-  "#cfe8f3",
-  "#73bfe2",
-  "#1696d2",
-  "#0a4c6a",
-  "#000000")
+cori_blue <- c(`Rin Blue` = "#244FBF",
+  `Light Blue` = "#B3CEF9")
 
 #' @rdname urbn_palettes
 #' @export
-palette_urbn_politics <- c(
-  "#1696d2",
-  "#db2b27")
+cori_purple<- c(`Rin Purple` = "#48336A",
+  `Light Purple` = "#CBBEDC")
 
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_cyan <- c(
-  "#cfe8f3",
-  "#a2d4ec",
-  "#73bfe2",
-  "#46abdb",
-  "#1696d2",
-  "#12719e",
-  "#0a4c6a",
-  "#062635")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_gray <- c(
-  "#f5f5f5",
-  "#ececec",
-  "#e3e3e3",
-  "#dcdbdb",
-  "#d2d2d2",
-  "#9d9d9d",
-  "#696969",
-  "#353535")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_yellow <- c(
-  "#fff2cf",
-  "#fce39e",
-  "#fdd870",
-  "#fccb41",
-  "#fabe15",
-  "#bb8e2d",
-  "#7f6127",
-  "#3e3215")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_magenta <- c(
-  "#f5cbdf",
-  "#eb99c2",
-  "#e46aa7",
-  "#e54096",
-  "#e90989",
-  "#af1f6b",
-  "#761548",
-  "#351123")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_green <- c(
-  "#dcedd9",
-  "#bcdeb4",
-  "#98cf90",
-  "#78c26d",
-  "#55b748",
-  "#408941",
-  "#2c5c2d",
-  "#1a2e19")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_spacegray <- c(
-  "#d5d5d4",
-  "#adabac",
-  "#848081",
-  "#5c5859",
-  "#332d2f",
-  "#262223",
-  "#1a1717",
-  "#0e0c0d")
-
-#' @rdname urbn_palettes
-#' @export
-palette_urbn_red <- c(
-  "#f8d5d4",
-  "#f1aaa9",
-  "#e9807d",
-  "#e25552",
-  "#db2b27",
-  "#a4201d",
-  "#6e1614",
-  "#370b0a")
