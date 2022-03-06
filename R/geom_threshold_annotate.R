7e04e3751fc4043ec3b04b4967a06429955749d4
gupta <>
2022-03-06 14:51:42 -0500

    Add geom_threshold_annotatae

---
+++ R/geom_threshold_annotate.R
@@ -0,0 +1,23 @@
+#' geom_threshold_annotate
+#'
+#' This is a description
+#' @param intercept x or y intercept (indicated by axis) through which the vertical or horizontal threshold is made.
+#' @param axis Axis along which a threshold is to be added. Default is "x" indicating a vertical threshold
+#' @param label Label to add to the graph. Defaults to an empty string
+#' @param shift Variable to shift the label by "shift" units above thresholding line along "y", or by "shift" units to the right of thresholding line along "x"
+#' @export
+geom_threshold_annotate <- function(intercept,axis="x",label="", shift=0){
+
+  if(axis == "x"){
+    list(
+      geom_vline(xintercept = intercept),
+      annotate(geom = 'text',x=intercept+shift,y=Inf, label=label, color="#06BCCB", vjust="top"))
+  } else {
+    list(
+      geom_hline(yintercept = intercept),
+      annotate(geom = 'text',y=intercept+shift,x=Inf, label=label, color="#06BCCB", hjust="right", size = 10.5))
+  }
+}
+
+
+
