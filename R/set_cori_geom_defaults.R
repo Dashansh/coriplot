315afd75a1b8aff29cb553487156fe081bf6f3b9
Dashansh <dashansh@yahoo.in>
2022-03-06 04:14:01 -0500

    Fixed conflicts with coriplot

---
+++ R/set_cori_geom_defaults.R
@@ -0,0 +1,24 @@
+#' The Urban Institute [ggplot2] theme
+#'
+#' \code{set_cori_geom_defaults} provides a [ggplot2] theme formatted according to the
+#' Urban Institute style guide, with sensible defaults.
+#'
+#' @param base_family The base font family for the theme.
+#'
+#' @export
+#'
+set_cori_geom_defaults <- function(base_family = "TT Hoves") {
+
+  # add base_family font to text and label geoms ---------------------------
+
+  ggplot2::update_geom_defaults("text", list(family = base_family, size = 1 / 0.352777778))
+  ggplot2::update_geom_defaults("label", list(family = base_family, size = 1 / 0.352777778))
+
+  # set defaults for geoms --------------------------------------------------
+
+  ggplot2::update_geom_defaults("point", list(colour = "#00835D", size = 2.5))
+  ggplot2::update_geom_defaults("line", list(colour = "#00835D", size = 1))
+  ggplot2::update_geom_defaults("vline", list(colour = "#D2D6D7", size = 0.264))
+  ggplot2::update_geom_defaults("hline", list(colour = "#D2D6D7", size = 0.264))
+
+}
